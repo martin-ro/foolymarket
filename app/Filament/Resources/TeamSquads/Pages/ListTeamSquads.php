dc70177697b48fe38832416cdad921d21bd215f1
Martin Ro <martin-ro@users.noreply.github.com>
2026-03-25 14:46:55 +0800

    wip

---
+++ app/Filament/Resources/TeamSquads/Pages/ListTeamSquads.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\TeamSquads\Pages;
+
+use App\Filament\Resources\TeamSquads\TeamSquadResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListTeamSquads extends ListRecords
+{
+    protected static string $resource = TeamSquadResource::class;
+}

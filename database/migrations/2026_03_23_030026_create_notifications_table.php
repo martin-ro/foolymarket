c318355d632d58107d539cd8475cb28142a7fdc7
Martin Ro <martin-ro@users.noreply.github.com>
2026-03-25 14:46:03 +0800

    wip

--- database/migrations/2026_03_23_030026_create_notifications_table.php
+++ database/migrations/2026_03_23_030026_create_notifications_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
@@ -19,13 +16,5 @@ return new class extends Migration
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('notifications');
     }
 };

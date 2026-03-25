62d8a162b22ca87699a60d9c6e96ea677a26f5f2
Martin Ro <martin-ro@users.noreply.github.com>
2026-03-25 08:47:02 +0800

    wip

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers\Filament;
 
+use AchyutN\FilamentLogViewer\FilamentLogViewer;
+use App\Filament\Pages\Dashboard;
+use Daljo25\FilamentTablerIcons\Enums\TablerIcon;
 use DutchCodingCompany\FilamentDeveloperLogins\FilamentDeveloperLoginsPlugin;
 use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
@@ -12,11 +15,12 @@ use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages\BasePage;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Enums\ColumnManagerLayout;
 use Filament\Tables\Table;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -34,6 +38,7 @@ class AppPanelProvider extends PanelProvider
             ->default()
             ->id('app')
             ->path('/')
+            ->viteTheme('resources/css/filament/app/theme.css')
             ->login()
             ->registration(false)
             ->passwordReset()
@@ -45,10 +50,20 @@ class AppPanelProvider extends PanelProvider
             ->databaseNotifications()
             ->sidebarCollapsibleOnDesktop()
             ->navigationGroups([
+                NavigationGroup::make('Matches'),
+                NavigationGroup::make('Data'),
                 NavigationGroup::make('Other'),
+                NavigationGroup::make('System'),
+            ])
+            ->navigationItems([
+                NavigationItem::make('Horizon')
+                    ->group('System')
+                    ->url(url: config('app.url').'/horizon', shouldOpenInNewTab: true)
+                    ->icon(TablerIcon::DatabaseImport),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->discoverPages(in: app_path('Filament/Clusters'), for: 'App\Filament\Clusters')
             ->pages([
                 Dashboard::class,
             ])
@@ -61,17 +76,29 @@ class AppPanelProvider extends PanelProvider
                     ->enabled(app()->environment('local'))
                     ->users(['test' => 'test@example.com'])
                     ->switchable(false),
+
+                FilamentLogViewer::make()
+                    ->navigationGroup('System'),
             ])
             ->bootUsing(function (): void {
-                // Striped tables
-                Table::configureUsing(fn (Table $table): Table => $table->deferLoading());
+                Table::configureUsing(fn (Table $table): Table => $table->striped());
                 Table::configureUsing(fn (Table $table): Table => $table->columnManagerLayout(ColumnManagerLayout::Modal));
                 Table::configureUsing(fn (Table $table): Table => $table->columnManagerColumns(2));
 
-                // Actions on right side
+                TextColumn::configureUsing(fn (TextColumn $column): TextColumn => $column
+                    ->sortable()
+                    ->searchable()
+                    ->label(function (TextColumn $column): string {
+                        return str($column->getName())
+                            ->afterLast('.')
+                            ->kebab()
+                            ->replace(['-', '_'], ' ')
+                            ->ucwords();
+                    })
+                );
+
                 BasePage::alignFormActionsEnd();
 
-                // Set slide-over as default for actions
                 CreateAction::configureUsing(fn (Action $action): Action => $action->slideOver());
                 EditAction::configureUsing(fn (Action $action): Action => $action->slideOver());
                 ViewAction::configureUsing(fn (Action $action): Action => $action->slideOver());
